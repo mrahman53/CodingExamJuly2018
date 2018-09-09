a23aa20586be5a2edea2a7b1d2d73484bef9df88
mrahman <rahmanww@gmail.com>
2018-09-09 01:19:25 -0400

    db update

---
+++ src/math/problem/Fibonacci.java
@@ -0,0 +1,11 @@
+package math.problem;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+         /*
+          Write 40 Fibonacci numbers with java.
+         */
+
+
+    }
+}
